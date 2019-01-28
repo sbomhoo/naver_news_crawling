1aebf5a439c7ccc8bc1f404f4394ef1e02686136
unknown <shinn0608@naver.com>
2019-01-28 16:16:43 +0900

    Add Success

---
+++ naver_news_crawling_02.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import pandas as pd
+import re
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+< naver 뉴스 검색시 리스트 크롤링하는 프로그램 > _select사용
+- 크롤링 해오는 것 : 링크,제목,신문사,날짜,내용요약본
+- 날짜,내용요약본  -> 정제 작업 필요
+- 리스트 -> 딕셔너리 -> df -> 엑셀로 저장 
+'''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+#각 크롤링 결과 저장하기 위한 리스트 선언 
+title_text=[]
+link_text=[]
+source_text=[]
+date_text=[]
+contents_text=[]
+result={}
+
+#엑셀로 저장하기 위한 변수
+RESULT_PATH ='C:/Users/User/Desktop/python study/beautifulSoup_ws/crawling_result/'  #결과 저장할 경로
+now = datetime.now() #파일이름 현 시간으로 저장하기
+
+#날짜 정제화 함수
+def date_cleansing(test):
+    try:
+        #지난 뉴스
+        #머니투데이  10면1단  2018.11.05.  네이버뉴스   보내기  
+        pattern = '\d+.(\d+).(\d+).'  #정규표현식 
+    
+        r = re.compile(pattern)
+        match = r.search(test).group(0)  # 2018.11.05.
+        date_text.append(match)
+        
+    except AttributeError:
+        #최근 뉴스
+        #이데일리  1시간 전  네이버뉴스   보내기  
+        pattern = '\w* (\d\w*)'     #정규표현식 
+        
+        r = re.compile(pattern)
+        match = r.search(test).group(1)
+        #print(match)
+        date_text.append(match)
+
+
+#내용 정제화 함수 
+def contents_cleansing(contents):
+    first_cleansing_contents = re.sub('<dl>.*?</a> </div> </dd> <dd>', '', 
+                                      str(contents)).strip()  #앞에 필요없는 부분 제거
+    second_cleansing_contents = re.sub('<ul class="relation_lst">.*?</dd>', '', 
+                                       first_cleansing_contents).strip()#뒤에 필요없는 부분 제거 (새끼 기사)
+    third_cleansing_contents = re.sub('<.+?>', '', second_cleansing_contents).strip()
+    contents_text.append(third_cleansing_contents)
+    #print(contents_text)
+    
+
+def crawler(maxpage,query,sort,s_date,e_date):
+
+    s_from = s_date.replace(".","")
+    e_to = e_date.replace(".","")
+    page = 1  
+  
+    while page <= int(maxpage):
+        url = "https://search.naver.com/search.naver?where=news&query=" + query + "&sort="+sort+"&ds=" + s_date + "&de=" + e_date + "&nso=so%3Ar%2Cp%3Afrom" + s_from + "to" + e_to + "%2Ca%3A&start=" + str(page)
+        
+        response = requests.get(url)
+        html = response.text
+ 
+        #뷰티풀소프의 인자값 지정
+        soup = BeautifulSoup(html, 'html.parser')
+ 
+        #<a>태그에서 제목과 링크주소 추출
+        atags = soup.select('._sp_each_title')
+        for atag in atags:
+            title_text.append(atag.text)     #제목
+            link_text.append(atag['href'])   #링크주소
+            
+        #신문사 추출
+        source_lists = soup.select('._sp_each_source')
+        for source_list in source_lists:
+            source_text.append(source_list.text)    #신문사
+        
+        #날짜 추출 
+        date_lists = soup.select('.txt_inline')
+        for date_list in date_lists:
+            test=date_list.text   
+            date_cleansing(test)  #날짜 정제 함수사용 
+        
+        #본문요약본
+        contents_lists = soup.select('ul.type01 dl')
+        for contents_list in contents_lists:
+            #print('==='*40)
+            #print(contents_list)
+            contents_cleansing(contents_list) #본문요약 정제화
+        
+        
+        #모든 리스트 딕셔너리형태로 저장
+        result= {"title":title_text , "link":link_text , "source" : source_text ,"date" : date_text ,"contents": contents_text }  
+        print(page)
+        
+        df = pd.DataFrame(result)  #df로 변환
+        page += 10
+    
+    
+    # 새로 만들 파일이름 지정
+    outputFileName = '%s-%s-%s  %s시 %s분 %s초 merging.xlsx' % (now.year, now.month, now.day, now.hour, now.minute, now.second)
+    df.to_excel(RESULT_PATH+outputFileName,sheet_name='sheet1')
+    
+    
+
+def main():
+    info_main = input("="*50+"\n"+"입력 방식은 main()함수의 주석 참고 해주세요."+"\n"+" 시작하시려면 Enter를 눌러주세요."+"\n"+"="*50)
+    
+    maxpage = input("페이지수 입력하시오: ")  #1페이지=1  2페이지=11  3페이지=21  4페이지=31 ....10페이지=91
+    query = input("검색어 입력: ")  
+    sort = input("뉴스 검색 방식 입력: ")    #관련도순=0  최신순=1  오래된순=2
+    s_date = input("시작날짜 입력:")  #2019.01.04
+    e_date = input("끝날짜 입력:")   #2019.01.05
+    
+    crawler(maxpage,query,sort,s_date,e_date) #검색된 네이버뉴스의 기사내용을 크롤링합니다. 페이지의 숫자를 넣으시오.
+    
+main()
+
+
+
+
+
+
+
+
+
+
+
+
